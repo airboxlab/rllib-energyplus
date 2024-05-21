4e21a32eee9834955879db8a92022642f844f421
antoine_galataud <antoine@foobot.io>
2024-05-21 14:00:04 +0200

    fix test

--- tests/test_train.py
+++ tests/test_train.py
@@ -10,5 +10,4 @@ class TestTrain(unittest.TestCase):
         self.assertEqual(args.timesteps, 1e6)
         self.assertEqual(args.num_workers, 2)
         self.assertEqual(args.num_gpus, 0)
-        self.assertEqual(args.alg, "PPO")
         self.assertFalse(args.use_lstm)
